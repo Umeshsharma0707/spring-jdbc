72fc015414e59f10306ce23179133a902ba11911
umesh sharma <umeshsharma07072003@gmail.com>
2024-06-15 23:22:06 +0530

    spring jdbc operations

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,5 +1,9 @@
 package com.spring.jdbc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       ApplicationContext context = 
+    		   new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+       
+      JdbcTemplate template =  context.getBean("jdbcTemplate", JdbcTemplate.class);
+      
+      // insert query
+      
+      String query = "insert into student (rollno, name, city) values (?,?,?)";
+       
+      // firing query
+      
+      int result = template.update(query, 12,"mahesh","delhi");
+      
+      System.out.println("no of rows inserted " + result);
+      
     }
 }
